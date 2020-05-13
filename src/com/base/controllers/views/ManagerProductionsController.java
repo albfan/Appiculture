707364d9a7f7997bb36e3ff4aeb1015af742748c
preantas <tranquiloteov@gmail.com>
2020-05-13 20:31:57 +0200

    bug fixes and implementations

---
+++ src/com/base/controllers/views/ManagerProductionsController.java
@@ -0,0 +1,14 @@
+package com.base.controllers.views;
+
+import com.base.models.structure.BaseController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManagerProductionsController extends BaseController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
